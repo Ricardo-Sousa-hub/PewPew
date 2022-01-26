2b6f80d96d33189da840c81a591aeeee4fb02fbb
Ricardo Sousa <satao.net@hotmail.com>
2022-01-26 15:53:54 +0000

    Correção de bug
    Mouse dava lock ao centro do ecrã, agora dá lock a janela.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -40,7 +40,7 @@ public class GameManager : MonoBehaviour
     {
         time = tempoEntreWaves;
 
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Confined;
 
         nightMode = PlayerPrefs.GetInt("Modo");
         if (nightMode == 0)
